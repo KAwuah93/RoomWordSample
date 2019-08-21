4b35d3f9f4c2fe5295bb275bd28a7f9a3609e23d
Kawuah93 <baffour.awuah@mobileappscompany.us>
2019-08-20 23:00:42 -0400

    First Commit
    Word Lab (with Room tech)

---
+++ app/src/main/java/com/example/roomwordsample/Word.java
@@ -0,0 +1,22 @@
+package com.example.roomwordsample;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    //we are annotating the class to make it more useful for the layer that handles the database
+    //entry points
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(String word) {this.mWord = word;}
+
+    public String getWord(){return this.mWord;}
+}
